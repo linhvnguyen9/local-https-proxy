51ff234bda5f66c6450d7fb5de832bcc350bf2e5
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-11-22 22:46:46 +0700

    Config for IPFS test

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: 'https://192.168.1.109:8090/'
   },
   devServer: {
-    contentBase: './dist'
+    https: true,
+    host: '192.168.1.109',//your ip address
+    port: 8090,
+    contentBase: './dist',
+    proxy: {
+      '/ipfs/*': {
+        target: 'http://localhost:8080/'
+      }
+    }
   }
 };
